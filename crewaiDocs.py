3e2b6cfcb4d52bb71c96b5383f68a9a7eb941f6d
Fabio Ramos <fabiowilliam466@gmail.com>
2024-09-30 10:23:54 -0400

    teste

--- crewaiDocs.py
+++ crewaiDocs.py
@@ -13,18 +13,18 @@ youtube_tool = YoutubeVideoSearchTool()
 serper_dev = SerperDevTool()
 claude_tool = call_claude_api
 
-documentation_specialist  = Agent(
-    role='Especialista em documentação',
-    goal='Criar uma documentação profissional utilizando tópicos e bullet points, adicionar bastante detalhes e um resumo final como conclusão.',
-    backstory="Especialista em documentação com grande experiencia, dominio em Markdown avançado e escritor de livros sobre Programação WEB.",
+extrator_de_urls  = Agent(
+    role='Responsável por extrair URLs de um site.',
+    goal='Fazer o crawling do site alvo e extrair todas as URLs válidas, excluindo links externos e arquivos de mídia.',
+    backstory="Especialista em web crawling com profundo entendimento das estruturas da web, capaz de navegar eficientemente pelo DOM para extrair informações necessárias, evitando links inválidos ou redundantes.",
     memory=False,
-    verbose=False,
+    verbose=True,
     model='gpt-4o-mini',
     allow_delegation = False
 )
 
-ai_internet_researcher = Agent(
-    role='Pesquisador de conteudos na internet',
+agente_de_filtragem = Agent(
+    role='Filtrar as URLs extraídas com base em critérios específicos.',
     goal='Encontrar conteúdos na internet sobre o tema abordado para complementarmos a documentação',
     backstory="Pesquisador senior com vasta experiencia em buscas avançadas de conteudo na internet.",
     memory=False,
@@ -35,8 +35,8 @@ ai_internet_researcher = Agent(
     tools=[serper_dev]
 )
 
-doc_reviewer  = Agent(
-    role='Revisor de documentos',
+agente_buscador  = Agent(
+    role='Buscar Imagens sem alt Text',
     goal='Revisar a documentação gerada pelos Agentes e gerar um documento unico em Markdown',
     backstory="Especialista em revisão de documentos e livros, dominio em Markdown e como deixar um visual mais agradavel",
     memory=False,
@@ -46,17 +46,17 @@ doc_reviewer  = Agent(
     allow_delegation = False
 )
 
-databricks_specialist  = Agent(
-    role='Especialista em Databricks',
-    goal='Validar a documentação gerada pelo agente doc_reviewer se esta de facil entendimento e se pode haver algum erro tecnico, se possivel complementar tecnicamente',
-    backstory="Especialista em Databricks com bastante experiencia em todos os tipos de cluster, tambem é muito bom em documentação e Markdown",
-    memory=False,
-    verbose=True,
-    max_iter=10,
-    allow_delegation = False,
-    model='gpt-4o-mini'
-    #tools=[claude_tool]
-)
+# agente_de_sugestao  = Agent(
+#     role='Analisar as Imagens anexadas e sugerir melhorias ou revisões para os links encontrados.',
+#     goal='Propor ajustes baseados na análise das URLs, seja removendo links desatualizados ou propondo melhores fontes.',
+#     backstory="Experiente em análise de conteúdo da web e melhoria contínua, sugerindo sempre as melhores práticas para manter a qualidade da documentação e análise.",
+#     memory=False,
+#     verbose=True,
+#     max_iter=10,
+#     allow_delegation = False,
+#     model='gpt-4o-mini'
+#     #tools=[claude_tool]
+# )
 
 task_documentation = Task(
     description="Documentar a transcrição a seguir: {transcript}",
